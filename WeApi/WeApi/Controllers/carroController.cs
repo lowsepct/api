d066630e7b6b5be66f45944008bcc19c76d90257
lowsepct <75146636+lowsepct@users.noreply.github.com>
2020-11-27 19:29:02 -0600

    Add files via upload

---
+++ WeApi/WeApi/Controllers/carroController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WeApi.Models;
+
+namespace WeApi.Controllers
+{
+    public class carroController : ApiController
+    {
+
+
+        Carro[] carros = new Carro[]{
+       
+       new Carro{idcarro=1,marca="Ferrari",modelo=2012},
+       new Carro{idcarro=2,marca="BMW",modelo=2010},
+       new Carro{idcarro=3,marca="Mazda",modelo=2002},
+       new Carro{idcarro=4,marca="Nizzan",modelo=2004},
+       new Carro{idcarro=5,marca="Renault",modelo=1998}
+       
+       };
+
+
+        public IEnumerable<Carro> GetAllCarro() {
+            return carros;
+        }
+
+
+        public IHttpActionResult GetCarro(int id) {
+            var carro = carros.FirstOrDefault((c)=>c.idcarro==id);
+            if (carro!=null)
+            {
+                return Ok(carro);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+
+
+    }
+}
---
+++ WeApi/WeApi/Models/Carro.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WeApi.Models
+{
+    public class Carro
+    {
+
+
+        public int idcarro { get; set; }
+        public string marca { get; set; }
+        public int modelo { get; set; }
+
+       
+
+    }
+}
